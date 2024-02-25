0c188b4db01d6cc323359cfc36a55c09647bd6ed
luping.bi <2631132715@qq.com>
2024-02-25 11:46:37 +0800

    feat(): 开始结束控制

--- src/main/kotlin/org/myboy/frame/GreedySnakePanel.kt
+++ src/main/kotlin/org/myboy/frame/GreedySnakePanel.kt
@@ -1,11 +1,14 @@
 package org.myboy.frame
 
 import org.myboy.food.SnakeFood
+import org.myboy.frameHeight
+import org.myboy.frameWidth
 import org.myboy.listener.SnakeKeyListener
 import org.myboy.refreshRate
 import org.myboy.snake.Snake
 import java.awt.Color
 import java.awt.Dimension
+import java.awt.Font
 import java.awt.Graphics
 import javax.swing.JPanel
 import javax.swing.Timer
@@ -14,24 +17,36 @@ class GreedySnakePanel : JPanel() {
 
     private val snake = Snake()
     private val food = SnakeFood()
+    private var isStart = false
 
     init {
         background = Color(0x3a3a3a)
         preferredSize = Dimension(800, 600)
         // 只有设置了聚焦，键盘事件才能触发
         isFocusable = true
-        addKeyListener(SnakeKeyListener(snake))
+        addKeyListener(SnakeKeyListener(snake, this))
         Timer(1000 / refreshRate) { repaint() }.start()
     }
 
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
+        if (!isStart) {
+            g.color = Color.white
+            g.font = Font("微软雅黑", Font.BOLD, 40)
+            g.drawString("点击空格开始游戏", frameWidth / 2 - 160, frameHeight / 2 - 20)
+        } else {
+            snake.move()
+        }
         food.paintFood(g)
-        snake.move()
         if (snake.isEatSuccess(food)) {
             snake.eatFood()
             food.generateFood()
         }
         snake.paintSnake(g)
     }
+
+    fun startOrStop() {
+        isStart = !isStart
+    }
+
 }
